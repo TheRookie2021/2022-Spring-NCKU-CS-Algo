002d3e08e0db25f043fb404d82b09bfbff8550c3
TheRookie2021 <leewilliamfish12@gmail.com>
2022-04-04 19:36:35 +0800

    hw5-3, v2

--- CourseHW/hw5_3/choose_kth_subset.cpp
+++ CourseHW/hw5_3/choose_kth_subset.cpp
@@ -1,31 +1,73 @@
-    #include<iostream>
-    #include<vector>
-    #include<set>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <bits/stdc++.h>
+using namespace std;
 
-    using namespace std;
+void judging(int *cocktails, int num, int num_of_judges, int kth)
+{
+    multiset<int> s;
+    multiset<int>::iterator last_output_itr;
+    int last_output = INT_MAX,last_deleted=INT_MAX;
+    for (int i = 0; i < num_of_judges - 1; i++)
+        s.insert(cocktails[i]);
 
-    void judging(int* cocktails,int num,int num_of_judges,int kth){
-        multiset<int> temp;
-        for (int i = 0; i < num - num_of_judges + 1; i++){
+    for (int i = num_of_judges - 1; i < num; i++)
+    {
+        // insert new: O(lg m)
+        s.insert(cocktails[i]);
 
-            // sort the range: m lg(m) 
-            // for (int j = 0; j < num_of_judges; j++){
-            //     temp.insert(cocktails[i+j]);
-            // }
-            temp.insert(cocktails+i,cocktails+(i+num_of_judges));
-            // output the kth small k
-            if(kth<num_of_judges/2){
-                multiset<int>::iterator itr=temp.begin();
-                for(int j=0;j<kth-1;j++,itr++);
-                cout<<*itr<<" ";
-
-            }else{
-                multiset<int>::reverse_iterator ritr = temp.rbegin();
-                for (int j = 0; j < num_of_judges - kth ; j++, ritr++);
-                cout<<*ritr<<" ";
-
-            }
+        // seek for the kth small k and output: O(m/2+2)
+        if ((cocktails[i] > last_output && last_deleted > last_output) ||
+            (cocktails[i] < last_output && last_deleted < last_output))
+        {
+            
+            //delete smaller, and input smaller, or delete larger, and input larger
+            cout << last_output<<" ";
         }
+        // else if (cocktails[i] > last_output && last_deleted < last_output)
+        // {
+        //     //delete smaller, and input larger: itr--
+        //     cout<<*(--last_output_itr)<<" ";
+
+        // }else if (cocktails[i] < last_output && last_deleted > last_output)
+        // {
+        //     //delete larger, and input smaller: itr--
+        //     cout<<*(++last_output_itr)<<" ";
+        // }
+        else
+        {
+            multiset<int>::iterator itr = s.begin();
+            for (int j = 0; j < kth - 1; j++, itr++);
+            cout << *itr << " ";
+            last_output = *itr;
+            last_output_itr=itr;
+        }
+
+        // for(auto i:s){
+        //     cout<<i<<" ";
+        // }
+        // cout<<endl;
+
+        // what if there are multiple elements with same value?
+        // can it erase exactly one of them?
+        last_deleted = cocktails[i - num_of_judges + 1];
+        if (s.count(last_deleted) > 1)
+        {
+            // O(lgm *1)
+            s.erase(s.find(last_deleted));
+        }
+        else
+        {
+            // O(lg m)
+            s.erase(last_deleted);
+        }
+        // for(auto i:s){
+        //     cout<<i<<" ";
+        // }
+        // cout<<endl;
+        // cout<<endl;
+    }
     }
 
     int main(){
