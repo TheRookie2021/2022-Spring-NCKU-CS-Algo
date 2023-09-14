30cb553837f0f4908ad95ab71dd13d8360b7b569
TheRookie2021 <csrookie2022@gmail.com>
2023-09-14 10:23:22 +0800

    change local

--- LeetCode/LeetCode 75/1071. Greatest Common Divisor of Strings/main.cpp
+++ LeetCode/LeetCode 75/1071. Greatest Common Divisor of Strings/main.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 #include<string>
-#include<unordered_set>
+#include<set>
 #include<algorithm>
+#include<vector>
 using namespace std;
 // practice the usage of string class
 string gcdOfStrings(string str1, string str2)
 {
-    if (str1[0] != str2[0])
-        return "";
+    // if (str1[0] != str2[0])
+    //     return "";
     
-    string gcd;
-    unordered_set<string> s_str1,s_str2;// not working,  
+    string gcd="";
+    set<string> s_str1,s_str2;// not working,  
     // unordered_set are not sorted in any particular order, but organized into buckets depending on their hash values to allow for fast access
 
     for(int i =0; i<str1.size();i++){
@@ -20,25 +21,20 @@ string gcdOfStrings(string str1, string str2)
         s_str2.insert(str2.substr(i,1));
         
     }
-    for (const std::string& x: s_str1) std::cout << " " << x;
-    cout<<endl;
-    if(s_str1==s_str2){
-        if(str1.size()>str2.size()){
-            for (auto it = s_str1.begin(); it != s_str1.end(); it++)
-            {
-                // cout<<*it;
-                gcd.append(*it);
-            }
-        }else{
-            for (auto it = s_str2.begin(); it != s_str2.end(); it++)
-            {
-                // cout<<*it;
-                gcd.append(*it);
-            }
+    vector<string> intersect_str(max(str1.size(),str2.size()));
+    auto it= set_intersection(s_str1.begin(),s_str1.end(),s_str2.begin(),s_str2.end(),intersect_str.begin());
+    intersect_str.resize(it - intersect_str.begin());
+    // if(s_str1==s_str2)
+    for (int i = 0; i < intersect_str.size(); i++) // we know the size of gcd by set container
+    {
+        if (str1[i] == str2[i]) // check the order of gcd (len of gcd must <= len of set)
+        {
+            gcd.append(str1.substr(i, 1));
         }
-        reverse(gcd.begin(),gcd.end());
-        return gcd;
-    }else return"";
+        else
+            break;
+    }
+    return gcd;
 }
 
 int main(){
---
+++ LeetCode/LeetCode 75/1071. Greatest Common Divisor of Strings/readme.md
@@ -0,0 +1,14 @@
+"TAUXXTAUXXTAUXXTAUXXTAUXX"
+"TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
+mine: "TAUX"
+expected ans: "TAUXX" 
+
+"ABCDE"
+"ABC"
+mine: "ABC"
+expected ans:  "" 
+
+
+if str1+str2==str2+str1
+    find the looping pattern in the sorter string
+else return ""
